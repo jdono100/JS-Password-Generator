31e03fe1546aba3cd401340c5bc5c9b1863e9c4a
Jared Donovan <jdono100@gmail.com>
2021-04-20 01:12:13 -0500

    pseudo-code in the js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Make an array with numbers, letters, and special characters
+// Have a prompt when generate is pressed to query the number of characters in the password
+  // Attempt to make it so that a minimum of numbers and letters are used, and only a small amount of special characters
+  // Have the prompt specify the range
+// Use math.random to randomize the array to generate the password with the correct number of characters
+// Have a "click to copy" text pop up only when a password has been generated
+// Add CSS styling and a footer if there is time
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
